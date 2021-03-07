acfca2c1c29bab165ba8ceb41c1198747d1f0471
Matthew <selectivelyusefull@gmail.com>
2021-03-07 13:09:09 -0500

    changed cm.c to use exec rather than system. Should work on windows now.

--- cm.c
+++ cm.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main (int argc, char *argv[]) {
     int status = 0;
     char arg[200];
+    char *args[] = {"racket", "-e", NULL, NULL};
     
     if (argc == 1) {
-        status = system ("racket -e \"(require cm/core/repl)\"");
+        args[2] = "(require cm/core/repl)";
     } else if (argc == 2) {
-        // treats argv[1] as a file and runs it
-        sprintf (arg, "racket -e '(require cm/core/cm) (cm-run-file \"%s\")'", argv[1]);
-        status = system (arg);
+        sprintf (arg, "(require cm/core/cm) (cm-run-file \"%s\")", argv[1]);
+        args[2] = arg;
     } else {
         printf ("Invalid number of args.\n");
         return 1;
     }
 
+    status = execvp (args[0], args);
     return status;
 
 }
