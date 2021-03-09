c7f3785e30e5de19ba437ffdfdafe714980fb210
Matthew Dolinka <mattdolinka1@gmail.com>
2021-03-09 18:05:05 -0500

    windows proofed the cm.c file

--- cm.c
+++ cm.c
@@ -3,25 +3,40 @@
 #include <unistd.h>
 #include <string.h>
 
+char* replacechr (char* chs, char targ, char rep);
+
 int main (int argc, char *argv[]) {
     int status = 0;
-    char arg[200];
-    char *args[] = {"racket", "-e", NULL, NULL};
+    char query[300];
     
     if (argc == 1) {
-        args[2] = "(require cm/core/repl)";
+        strcpy (query, "racket -e \"(require cm/core/repl)\"");
     } else if (argc == 2) {
-        sprintf (arg, "(require cm/core/cm) (cm-run-file \"%s\")", argv[1]);
-        args[2] = arg;
+        // act like path is unix
+        strcpy (argv[1], (replacechr (argv[1], '\\', '/')));
+        sprintf (query,
+                "racket -e \"(require cm/core/cm) (cm-run-file \\\"%s\\\")\"",
+                argv[1]);
     } else if (argc == 3 && !(strcmp (argv[1], "-s"))) {
-        sprintf (arg, "(require cm/core/cm) (cm-run-file-silent \"%s\")", argv[2]);
-        args[2] = arg;
+        strcpy (argv[2], (replacechr (argv[2], '\\', '/')));
+        sprintf (query,
+                "racket -e \"(require cm/core/cm) (cm-run-file-silent \\\"%s\\\")\"",
+                argv[2]);
     } else {
         printf ("Invalid args.\n");
         return 1;
     }
-
-    status = execvp (args[0], args);
+    
+    status = system (query);
     return status;
 
+}
+
+// replaces all instances of targ in chs with rep
+char* replacechr (char* chs, char targ, char rep) {
+    int i = 0;
+    for (i = 0; chs[i]; i++) {
+        if (chs[i] == targ) chs[i] = rep;        
+    }
+    return chs;
 }
