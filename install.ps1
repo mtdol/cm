b754bf8fc340baa28128b8ffe3f2cc3e1e19ed50
Matthew Dolinka <mattdolinka1@gmail.com>
2021-04-07 18:57:01 -0400

    Adapted windows installer to act the same as the bash one.

--- install.ps1
+++ install.ps1
@@ -1,2 +1,9 @@
-$std_loc = join-path $PWD std_lib\
-set-content ./config/modules.txt "# this is a comment`n""std_lib"":""$std_loc"""
+$cm_loc = $PSScriptRoot
+$std_loc = join-path $cm_loc std_lib\
+
+# set the location of the cm directory
+set-content (join-path $cm_loc config\ cm_loc.txt) `
+    $cm_loc
+# create a basic modules file
+set-content (join-path $cm_loc config\ modules.txt) `
+    "# this is a comment`n""std_lib"":""$std_loc"""
