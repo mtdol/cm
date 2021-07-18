00106cc38be9a37f9dfe6d747c6983383c3db848
Matthew <selectivelyusefull@gmail.com>
2021-07-17 22:16:09 -0400

    Added an array map function

--- tests/interp/array2.rkt
+++ tests/interp/array2.rkt
@@ -28,3 +28,20 @@ val-void)
 2)
 (check-equal? (run "b::3")
 1)
+
+;; array_map
+
+(run-silent "def a := list_to_array:{list 1|2|3}")
+
+(check-equal? (run "array_map : add1 : a")
+val-void)
+
+(check-equal? (run "a::0")
+2)
+(check-equal? (run "a::1")
+3)
+(check-equal? (run "a::2")
+4)
+
+(check-equal? (run "array_map : add1 : (make_array : 0)")
+val-void)
