446f80fb990fcadfe2549ec6e9c4129c976e8cf0
Matthew <selectivelyusefull@gmail.com>
2021-07-17 20:42:18 -0400

    Added a zip function to std

--- tests/std_lib/std/math2.rkt
+++ tests/std_lib/std/math2.rkt
@@ -58,6 +58,35 @@
 '((1 3) (5 1)))
 
 ;;
+;; zip
+;;
+
+(check-equal? (run "zip : {list} : {list}")
+'())
+
+(check-equal? (run "zip : {list} : {list 1}")
+'())
+
+(check-equal? (run "zip : {list 1} : {list}")
+'())
+
+(check-equal? (run "zip : {list 1} : {list 2}")
+'((1 . 2)))
+
+(check-equal? (run "zip : {list 1} : {list 2|4}")
+'((1 . 2)))
+
+(check-equal? (run "zip : {list 1|3} : {list 2|4}")
+'((1 . 2) (3 . 4)))
+
+(check-equal? (run "zip : {list 1|3} : {list 2|4|6}")
+'((1 . 2) (3 . 4)))
+
+(check-equal? (run "zip : {list 1|3|5} : {list 2|4|6}")
+'((1 . 2) (3 . 4) (5 . 6)))
+
+
+;;
 ;; min, max
 ;;
 
